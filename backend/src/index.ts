2a506b98cd261c391a583d1384cb5c7f941c6be0
Anthony <anthonysalisbury98@outlook.com>
2025-10-11 21:40:58 +0100

    feat(backend): implement basic rate limiting with express-rate-limit library

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,11 +4,17 @@ import initialisePassport from "./passport/initialisePassport";
 import session from "express-session";
 import bodyParser from "body-parser";
 import routes from "./routes/indexRoutes";
+import rateLimit from "express-rate-limit";
 
 import { authenticateJWT, AuthRequest } from "./middlewares/authenticateJWT";
 
 const app = express();
 const PORT = process.env.PORT || 4000;
+const limter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+  message: "Too many requests from this IP, please try again after 15 minutes",
+})
 
 initialisePassport();
 
@@ -28,6 +34,7 @@ app.use(
 app.use(express.urlencoded({ extended: false }));
 app.use(passport.initialize());
 app.use(passport.session());
+app.use(limter);
 
 app.use("/api/v1", routes);
 
@@ -35,7 +42,6 @@ app.get("/api/profile", authenticateJWT, (req: Request, res: Response) => {
   const authReq = req as AuthRequest;
   res.json({ message: "Access granted to profile", userId: authReq.user?.id });
 });
-
 
 app.listen(PORT, () => {
   console.log(`Sever running on http://localhost:${PORT}`);
