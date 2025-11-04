ac3df955e59f6ef51246e937889bbf65a7da3aec
Anthony <anthonysalisbury98@outlook.com>
2025-11-04 00:09:14 +0000

    chore: setup React Router

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,16 @@
 import { Button, VStack } from "@chakra-ui/react"
+import { Link } from "react-router-dom"
 
 function App() {
   return (
     <>
       <VStack>
-        <Button>Click me</Button>
-        <Button>Click me</Button>
+        <Link to="/login">
+          <Button>Login</Button>
+        </Link>
+        <Link to="/sign-up">
+          <Button>Sign Up</Button>
+        </Link>
       </VStack>
     </>
   )
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,12 +1,21 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { Provider } from './components/ui/provider.tsx'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import App from './App.tsx'
+import Login from './pages/Login.tsx'
+import SignUp from './pages/SignUp.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Provider>
-      <App />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<App />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/sign-up" element={<SignUp />} />
+        </Routes>
+      </BrowserRouter>
     </Provider>
-  </StrictMode>,
+  </StrictMode>
 )
